3d5f067010c3079bfef7832d9165551fc876682e
RaissaBarbs <168589862+LuizEVE@users.noreply.github.com>
2025-02-13 11:32:40 -0300

    vfudi

--- BancoDeDados/BancoAtualizado.sql
+++ BancoDeDados/BancoAtualizado.sql
@@ -13,9 +13,73 @@ CREATE TABLE Produto (
     Descricao VARCHAR(100) NOT NULL,
     UnMedida VARCHAR(100) NOT NULL,
     EstoqueAtual FLOAT,
-    EPermanente BIT DEFAULT 0 -- Define 0 como valor padrão
+    EPermanente BIT DEFAULT 0, -- Define 0 como valor padrão
+    CodigoCategoria INT NULL
+);
+alter table Produto
+add foreign key (CodigoCategoria) references Categoria(Codigo)
+
+Create table Entrada(
+Codigo int identity(1,1) primary key,
+DataEntrada datetime,
+CodigoFronecedor int null,
+Observacao varchar(100)
+);
+alter table Entrada
+add foreign key (CodigoFronecedor) references Fornecedor(Codigo)
+
+CREATE TABLE ItensEntrada (
+    Codigo INT IDENTITY(1,1) PRIMARY KEY,
+    CodigoProduto INT NULL,
+    CodigoEntrada INT NULL,
+    Quantidade INT NOT NULL,
+    Preco FLOAT DEFAULT 0.00,
+	Total Float Default 0.00
+);
+alter table ItensEntrada
+add foreign key (CodigoProduto) references Produto(Codigo),
+foreign key (CodigoEntrada) references Entrada(Codigo)
+
+Create table Fornecedor(
+Codigo int identity(1,1) primary key,
+NomeFornecedor varchar(100) not null,
+Endereco varchar(100) not null,
+Bairro varchar(100) not null,
+Cidade varchar(100),
+Estado varchar(2),
+Telefone varchar(100),
+CNPJ varchar(100)
 );
 
-Create table (
-
+create table Secretaria(
+Codigo int identity(1,1) primary key,
+NomeSecretaria varchar(100),
+Endereco varchar(100),
+Bairro varchar(100),
+Cidade varchar(100),
+Estado varchar(2),
+Telefone Varchar(100),
+CNPJ varchar(100)
 );
+
+create table Saida (
+Codigo int identity(1,1) primary key,
+DataSaida datetime not null,
+CodigoSecretaria int null,
+Observacao varchar(100)
+);
+alter table Saida
+add foreign key (CodigoSecretaria) references Secretaria(Codigo)
+
+create table ItensSaida(
+Codigo int identity(1,1) primary key,
+CodigoProduto int null,
+CodigoSaida int null,
+Quantidade int not null,
+Proco float default 0.00,
+Total float default 0.00
+);
+alter table ItensSaida
+add foreign key (CodigoProduto) references Produto(Codigo),
+foreign key (CodigoSaida) references Saida(Codigo)
+
---
+++ BancoDeDados/Tabelas atualixadas.sql
@@ -0,0 +1,27 @@
+use DBAlmoxarifadoXYZ
+go
+
+select * from Categoria
+Select * from Produto
+Select * from Fornecedor
+Select * from Entrada
+Select * from ItensEntrada
+Select * from Secretaria
+Select * from Saida
+Select * from ItensSaida
+
+--Categoria
+Insert into Categoria(Descricao)
+values ('Alimentos')
+
+--Produto
+Insert into Produto(Descricao, UnMedida, EstoqueAtual, EPermanente, CodigoCategoria)
+values ('Arroz', 'Kilo', '500', 0, '1')
+
+--Fornecedor
+insert into Fornecedor(NomeFornecedor, Endereco, Bairro, Cidade, Estado, Telefone, CNPJ)
+values ('Luiz', 'Rua Caetano', 'Centro', 'Indiaroba', 'SE', '(79)9 9675-9541', '12.540.360/0001-21')
+
+--Secretaria
+insert into Secretaria(NomeSecretaria, Endereco, Bairro, Cidade, Estado, Telefone, CNPJ)
+values ('Destribuidora', 'Rua Alguma Coisa', 'Alagoas', 'Estancia', 'SE', '(79)9 9600-0290')
